83b9d3f451e6f08b6cc5f8b4b4aa005a409591a3
Abdul Quadir <aquadir83@gmail.com>
2020-02-20 15:20:00 +0530

    added Ngrx on the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { CovalentBaseEchartsModule } from '@covalent/echarts/base';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/effects/app.effects';
 
 
 
@@ -30,6 +36,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CovalentDynamicFormsModule,
     CovalentBaseEchartsModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent]
