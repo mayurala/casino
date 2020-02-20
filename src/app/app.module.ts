0cb517b96b0d1df6da2fb33ae1492d400b462074
Abdul Quadir <aquadir83@gmail.com>
2020-02-20 15:52:02 +0530

    Added Material and covalent to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { CovalentLayoutModule } from '@covalent/core/layout';
-import { CovalentStepsModule } from '@covalent/core/steps';
-import { CovalentHttpModule } from '@covalent/http';
-import { CovalentHighlightModule } from '@covalent/highlight';
-import { CovalentMarkdownModule } from '@covalent/markdown';
-import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
-import { CovalentBaseEchartsModule } from '@covalent/echarts/base';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +13,8 @@ import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './store/effects/app.effects';
 
-
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentStepsModule } from '@covalent/core/steps';
 
 @NgModule({
   declarations: [
@@ -30,11 +25,6 @@ import { AppEffects } from './store/effects/app.effects';
     AppRoutingModule,
     CovalentLayoutModule,
     CovalentStepsModule,
-    CovalentHttpModule.forRoot(),
-    CovalentHighlightModule,
-    CovalentMarkdownModule,
-    CovalentDynamicFormsModule,
-    CovalentBaseEchartsModule,
     BrowserAnimationsModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
