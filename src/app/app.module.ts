d9127835200f33cafeac9cdf4fdd6dc78ee3d01f
Abdul Quadir <aquadir83@gmail.com>
2020-02-25 17:03:10 +0530

    Adding Jest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import { AppEffects } from './store/effects/app.effects';
 
 import { CovalentLayoutModule } from '@covalent/core/layout';
 import { CovalentStepsModule } from '@covalent/core/steps';
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +30,11 @@ import { CovalentStepsModule } from '@covalent/core/steps';
     AppRoutingModule,
     CovalentLayoutModule,
     CovalentStepsModule,
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+
     BrowserAnimationsModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
