4c65ad0edad732b7872c7276ae434e5449ed1c3c
Abdul Quadir <aquadir83@gmail.com>
2020-02-26 12:46:07 +0530

    Fixed Failing Test cases on store.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    let store: MockStore<{ isLoading: boolean }>;
+    const initialState = { isLoading: false };
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -11,7 +16,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
+      // schemas: [NO_ERRORS_SCHEMA]
+
     }).compileComponents();
+
   }));
 
   test('should create the app', () => {
